0572cb5475ab9287762100ce349ab04c609fc943
tomemelko <tom.emelko@gmail.com>
2012-07-11 17:23:51 -0400

    Connects to server, only listens

--- nomad.py
+++ nomad.py
@@ -1,19 +1,39 @@
-import sys 
+#Loosely modeled from: http://www.osix.net/modules/article/?id=780
+import sys
 import socket 
-import string 
+import thread 
+import time
+
+def listen(socketInUse):
+  while True:
+    line = socketInUse.recv(4096)
+    if line != "":
+      if line != "\r\n":
+        print line
 
 #Config
-host = '127.0.0.1'
-port = '6667'
-nick = 'NOMAD'
+host = 'localhost'
+port = 6667
+nick = 'nomad'
 ident = 'nomadbot'
 realname = 'nomadbot'
 owner = 'tom'
 channel = '\#nomad'
 readbuffer = ''
 
-#Connect to server and identify
+#Connect to server and listen
 s = socket.socket()
-s.connect(host, port)
-s.send('NICK ' + nick + 'n')
-s.send('USER ' + ident + ' ' + host + ' bla :' + realname + 'n')
+thread.start_new_thread(listen, (s,))
+s.connect((host, port))
+time.sleep(2)
+print "Sending USER",ident,host,"bla :",realname
+s.send('USER '+ident+' '+host+' bla :'+realname+'\r\n') #Identify to server 
+time.sleep(5)
+print "Sending NICK",nick
+s.send('NICK ' + nick + "\r\n")
+time.sleep(5)
+print "Joining channel"
+s.send('JOIN #nomad \r\n')
+
+while True:
+  pass
