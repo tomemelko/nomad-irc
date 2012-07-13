8ed5cec5651bac215921baeb2c8b6047ef3f74a6
tomemelko <tom.emelko@gmail.com>
2012-07-13 00:49:44 -0400

    Works better, need to randomize sentence starts

--- markov.py
+++ markov.py
@@ -7,20 +7,27 @@ stopword = "\n" # Since we split on whitespace, this can never be a word
 stopsentence = (".", "!", "?",) # Cause a "new sentence" if found at the end of a word
 sentencesep  = "\n" #String used to seperate sentences
 
-
-# GENERATE TABLE
 w1 = stopword
 w2 = stopword
 table = {}
 
-for line in open("/home/tom/nltk_data/corpora/webtext/overheard_modified.txt"):
-  for word in line.split():
-    if word[-1] in stopsentence:
-      table.setdefault( (w1, w2), [] ).append(word[0:-1])
-      w1, w2 = w2, word[0:-1]
-      word = word[-1]
-    table.setdefault( (w1, w2), [] ).append(word)
-    w1, w2 = w2, word
+# GENERATE TABLE
+def load(filepath):
+  w1 = w2 = stopword
+  for line in open(filepath):
+    for word in line.split():
+      if word[-1] in stopsentence:
+        table.setdefault( (w1, w2), [] ).append(word[0:-1])
+        w1, w2 = w2, word[0:-1]
+        word = word[-1]
+      table.setdefault( (w1, w2), [] ).append(word)
+      w1, w2 = w2, word
+
+load("/home/tom/nltk_data/corpora/webtext/overheard_modified.txt")
+load("/home/tom/nltk_data/corpora/webtext/grail_modified.txt")
+load("/home/tom/nltk_data/corpora/webtext/wine.txt")
+load("/home/tom/nltk_data/corpora/webtext/firefox_modified.txt")
+
     
 # Mark the end of the file
 table.setdefault( (w1, w2), [] ).append(stopword)
@@ -38,13 +45,11 @@ while sentencecount < maxsentences:
   if newword == stopword: sys.exit()
   if newword in stopsentence:
     joined = " ".join(sentence)
-    if joined != ".":
-      if joined != "!":
-        if joined != "?":
-          print "%s%s%s" % (joined, newword, sentencesep)
-          sentencecount += 1
-    sentence = []
+    if (len(joined) > 2):
+      print joined, newword, sentencesep
+      sentencecount += 1
+      sentence = []
+      w1 = w2 = stopword
   else:
     sentence.append(newword)
     w1, w2 = w2, newword
-
