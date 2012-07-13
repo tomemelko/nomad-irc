33caf790854158cd64cc6b77ac4db6959c5d1f28
tomemelko <tom.emelko@gmail.com>
2012-07-13 01:13:34 -0400

    Switched to module structure

--- markov.py
+++ markov.py
@@ -10,29 +10,33 @@ w2 = nonword
 # GENERATE TABLE
 table = {}
 
-for line in open("/home/tom/nltk_data/corpora/webtext/overheard_modified.txt"):
-  for word in line.split():
-    table.setdefault( (w1, w2), [] ).append(word)
-    w1, w2 = w2, word
-                    
-table.setdefault( (w1, w2), [] ).append(nonword) # Mark the end of the file
-                    
-# GENERATE OUTPUT
-w1 = nonword
-w2 = nonword
+def load(filepath):
+  w1 = w2 = nonword
+  for line in open(filepath):
+    for word in line.split():
+      table.setdefault( (w1, w2), [] ).append(word)
+      w1, w2 = w2, word
+  table.setdefault( (w1, w2), [] ).append(nonword) # Mark the end of the file
 
-maxwords = 10000
-sentence = []
+def generate(files, number):                    
+  for file in files:
+    load(file)
+  # GENERATE OUTPUT
+  w1 = nonword
+  w2 = nonword
 
-for i in xrange(maxwords):
-  newword = random.choice(table[(w1, w2)])
-  if newword == nonword: sys.exit()
-  if newword[-1] in sentenceend:
-    joined = " ".join(sentence)
-    if len(joined) > 1:
-      print joined,newword
-      sentence = []
-  else:
-    sentence.append(newword)    
-  w1, w2 = w2, newword
-                                    
+  sentence = []
+  sentences = []
+  #for i in xrange(maxwords):
+  while len(sentences) < number:
+    newword = random.choice(table[(w1, w2)])
+    if newword == nonword: sys.exit()
+    if newword[-1] in sentenceend:
+      joined = " ".join(sentence)
+      if len(joined) > 1:
+        sentences.append(joined+" "+newword)
+        sentence = []
+    else:
+      sentence.append(newword)    
+    w1, w2 = w2, newword
+  return sentences
